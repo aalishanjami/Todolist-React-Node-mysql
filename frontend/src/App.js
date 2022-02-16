0e8140fb59b7b08f6bc6f03c5ed0adef24a532b0
Aalishan Jami <aalishanj1@gmail.com>
2022-02-17 03:58:03 +0500

    private ip

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,8 +8,7 @@ import Register from './components/Register'
 import Profile from './components/Profile'
 import TodoList from './components/TodoList'
 
-axios.defaults.baseURL = "http://100.26.113.191/backend";
-
+axios.defaults.baseURL = "http://10.0.0.118/backend";
 
 function App() {
   return (
